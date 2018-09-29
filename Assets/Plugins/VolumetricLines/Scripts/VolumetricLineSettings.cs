c573aafe4246abeb6a259050b95e9fd11daa1e68
wqferr <wqferr@gmail.com>
2018-09-29 10:36:36 -0300

    Create wall prefabs

---
+++ Assets/Plugins/VolumetricLines/Scripts/VolumetricLineSettings.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Global settings for Volumetric Lines rendering
+/// </summary>
+public class VolumetricLineSettings : MonoBehaviour
+{
+	/// <summary>
+	/// If set to true, volumetric lines rendering will not apply scaling based on the
+	/// camera's field of view.
+	/// By default (which means, this property is set to false), scaling is enabled,
+	/// which means, that for varying field of view values, volumetric lines will have 
+	/// a constant line width.
+	/// </summary>
+	#pragma warning disable 0649
+	[SerializeField]
+	private bool m_disableFieldOfViewScaling;
+	#pragma warning restore 0649
+
+
+	// Use this for initialization
+	void Awake ()
+	{
+		if (m_disableFieldOfViewScaling)
+		{
+			Shader.EnableKeyword("FOV_SCALING_OFF");
+		}	
+		else
+		{
+			Shader.DisableKeyword("FOV_SCALING_OFF");
+		}
+	}
+	
+}
