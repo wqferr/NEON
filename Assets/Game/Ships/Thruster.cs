859cb4d775e31b14ea8fad5c5e86578db0e7c9df
wqferr <wqferr@gmail.com>
2018-09-29 19:22:31 -0300

    Start movement test scene

---
+++ Assets/Game/Ships/Thruster.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace NeonChaser.Testing
+{
+	[RequireComponent(typeof(Rigidbody))]
+	public class Thruster : MonoBehaviour
+	{
+		private new Rigidbody rigidbody;
+		private new Transform transform;
+		public float maxForce = 3;
+		[SerializeField]
+		private float currentForce = 0;
+
+		void Awake()
+		{
+			this.rigidbody = GetComponent<Rigidbody>();
+			this.transform = GetComponent<Transform>();
+		}
+
+		void FixedUpdate()
+		{
+			var force = this.currentForce * this.transform.up;
+			this.rigidbody.AddForce(force);
+		}
+
+		void OnValidate()
+		{
+			SetForce(this.currentForce);
+		}
+
+		public void SetForce(float force)
+		{
+			if (force < 0)
+				force = 0;
+			if (force > this.maxForce)
+				force = this.maxForce;
+			this.currentForce = force;
+		}
+	}
+}
