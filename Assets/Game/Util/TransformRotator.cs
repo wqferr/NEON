5ec91179bac48672c409177cf89419deefd0773c
wqferr <wqferr@gmail.com>
2018-09-29 01:50:15 -0300

    Create enemies

---
+++ Assets/Game/Util/TransformRotator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace NeonChaser.Util
+{
+	public class TransformRotator : MonoBehaviour
+	{
+		public Vector3 rotationAxis = Vector3.up;
+		public Transform rotatedTransform;
+
+		public void SetRotation(float angle)
+		{
+			this.rotatedTransform.eulerAngles = Vector3.zero;
+			this.rotatedTransform.RotateAround(
+				this.rotatedTransform.position,
+				this.rotationAxis,
+				angle);
+		}
+	}
+}
